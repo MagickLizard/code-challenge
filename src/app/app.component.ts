5b236b6bf3504dc6dc61bb4484c2176edc516894
Lizzie King <lizzieking521@gmail.com>
2019-04-29 18:05:58 +1000

    ADDED: getbygenders functions and added bulma

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,51 +1,78 @@
-import {Component} from '@angular/core';
-import {RequestService} from './request-service.service';
-import { Observable, of } from 'rxjs';
- 
+import { Component } from '@angular/core';
+import { RequestService } from './request-service.service';
+
 @Component({
   selector: 'body',
-  template:`
-  <h2>People</h2>
-  <header *ngFor="let personGender of people">{{personGender.gender}}</header>
-  <ul>
-    <li *ngFor="let person of people">{{person.name}}</li>
-    <li *ngFor="let person of people">{{person.pets}}</li>
-    <li *ngFor="let person of people"><input type="text" name="person-name" [(ngModel)]="person"> </li>
-    <p>Create a new food: <input type="text" name="food_name" [(ngModel)]="food_name"><button (click)="createFood(food_name)">Save</button></p>
-  </ul>
+  template: `
+  <section class="hero is-primary">
+  <div class="hero-body">
+    <div class="container">
+      <h1 class="title">
+        Data about cat owners and there cats
+      </h1>
+    </div>
+  </div>
+</section>
+<section class="section">
+  <div class="container">
+    <br>
+    <div class="card-content">
+      <div class="content">
+        <h2>Pet owner gender and cat name:</h2>
+        <div class="columns">
+        <h2>Male</h2>
+          <ul *ngFor="let petNames of ownerMale">
+            <div class="column">
+              {{petNames}}
+            </div>
+          </ul>
+        </div>
+        <div class="columns">
+        <h2>Female</h2>
+          <ul *ngFor="let petNames of ownerFemale">
+            <div class="column">
+              {{petNames}}
+            </div>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
   `
 })
 export class AppComponent {
-//  people: People[];
-public people;
-public person;
- public pets;
-
+  public people;
+  public person;
+  public pets;
+  public ownerMale;
+  public ownerFemale;
 
   ngOnInit() {
     this.getPeople();
   }
-  // getPeople(): void {
-  //   this._requestService.getPeople().subscribe(
-  //      data => this.people = data
-  //    );
-  //  }
   getPeople() {
     this._requestService.getPeople().subscribe(
-       data => { this.people = data
-         console.log('data', data)
-         this.getCats(data);
-         return data;},
-       err => console.error(err),
-       () => console.log('done loading people')
-     );
-   }
-   getCats(person) {
-    console.log('person.person', person)
-    return this._requestService.getCats(person).subscribe(
-      data => data
-      );
-   }
- 
+      data => {
+      this.people = data
+       const catsFound = this.getCats(data);
+       this.ownerMale = this.getMaleOwners(catsFound);
+       this.ownerFemale = this.getFemaleOwners(catsFound);
+        // return this.pets;
+      },
+      err => console.error(err),
+      () => console.log('done loading pets')
+    );
+  }
+  getCats(person) {
+    return this._requestService.getCats(person)
+  }
+  getMaleOwners(catsArray) {
+    return this._requestService.getMaleOwners(catsArray)
+  }
+  getFemaleOwners(catsArray) {
+    return this._requestService.getFemaleOwners(catsArray)
+  }
+
   constructor(private _requestService: RequestService) { }
 }
--- src/app/request-service.service.ts
+++ src/app/request-service.service.ts
@@ -1,49 +1,49 @@
-import {Injectable} from '@angular/core';
+import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { People } from './People';
- 
+
+
 const httpOptions = {
-    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
 };
- 
+
 @Injectable()
 export class RequestService {
- 
-    constructor(private http:HttpClient) {}
- 
-    // Uses http.get() to load data from a single API endpoint
-    getPeople() {
-      console.log('hello')
-        let request = this.http.get('http://agl-developer-test.azurewebsites.net/people.json');
-        console.log('request>>', request)
-        return request;
-    }
-    getCats(people) {
-      const petsFiltered = people.filter(items => items && items.pets !== null && items.pets.length > 0)
-      let catArray = [];
-      (petsFiltered).reduce((previous, current) => {
-        for (let petObj of current.pets) {
-          if (petObj.type === 'Cat') {
-            catArray.push({ 
-              catDetails: petObj.name,
-              ownerInfo: current.gender
-            })
-          }
+
+  constructor(private http: HttpClient) { }
+
+  // Uses http.get() to load data from a single API endpoint
+  getPeople() {
+    let request = this.http.get('http://agl-developer-test.azurewebsites.net/people.json');
+    return request;
+  }
+  getCats(people) {
+    const petsFiltered = people.filter(items => items && items.pets !== null && items.pets.length > 0)
+let arrayOfPets = [];
+   (petsFiltered).map((i) => {
+      for (let petObj of i.pets) {
+        if (petObj.type === 'Cat') {
+           arrayOfPets.push({[i.gender] :petObj.name})
         }
-        return previous
-      }, {});
-      let sortedResult = catArray.sort((a, b) => {
-        var nameA = a.catDetails.toUpperCase(); 
-        var nameB = b.catDetails.toUpperCase();
-        if (nameA < nameB) {
-          return -1;
+      }
+  })
+  return arrayOfPets;
+  }
+  getMaleOwners(allArray) {
+    let maleArray = [];
+    (allArray).map((i) => {
+        if (i.Male) {
+          maleArray.push(i.Male);
         }
-        if (nameA > nameB) {
-          return 1;
+  })
+  return maleArray.sort()
+  }
+  getFemaleOwners(allArray) {
+    let femaleArray = [];
+    (allArray).map((i) => {
+        if (i.Female) {
+          femaleArray.push(i.Female);
         }
-        return 0;
-      });
-      console.log('sortedResult', sortedResult)
-      return sortedResult;
-    }
+  })
+  return femaleArray.sort()
+  }
 }
