57a0771b6730e6a998d5f9afcd3fe1cba3a35582
Lizzie King <lizzieking521@gmail.com>
2019-04-29 18:55:04 +1000

    UPDATED: clean up and formatted component body

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,50 +1,46 @@
-import { Component } from '@angular/core';
-import { RequestService } from './request-service.service';
+import { Component } from "@angular/core";
+import { RequestService } from "./request-service.service";
 
 @Component({
-  selector: 'body',
+  selector: "body",
   template: `
-  <section class="hero is-primary">
-  <div class="hero-body">
-    <div class="container">
-      <h1 class="title">
-        Data about cat owners and there cats
-      </h1>
-    </div>
-  </div>
-</section>
-<section class="section">
-  <div class="container">
-    <br>
-    <div class="card-content">
-      <div class="content">
-        <h2>Pet owner gender and cat name:</h2>
-        <div class="columns">
-        <h2>Male</h2>
-          <ul *ngFor="let petNames of ownerMale">
-            <div class="column">
-              {{petNames}}
-            </div>
-          </ul>
-        </div>
-        <div class="columns">
-        <h2>Female</h2>
-          <ul *ngFor="let petNames of ownerFemale">
-            <div class="column">
-              {{petNames}}
-            </div>
-          </ul>
+    <section class="hero is-primary">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">
+            Data about cat owners and there cats
+          </h1>
         </div>
       </div>
-    </div>
-  </div>
-</section>
+    </section>
+    <section class="section">
+      <div class="container">
+        <div class="columns">
+          <div class="column is-one-sixth">
+            <h2 class="title">Female pet owners</h2>
+            <div *ngFor="let petNames of ownerFemale">
+              <div>
+                Pet Name:
+                {{ petNames }}
+              </div>
+            </div>
+          </div>
+          <br />
+          <div class="column is-one-sixth">
+            <h2 class="title">Male pet owners</h2>
+            <div *ngFor="let petNames of ownerMale">
+              <div>
+                Pet Name:
+                {{ petNames }}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   `
 })
 export class AppComponent {
-  public people;
-  public person;
-  public pets;
   public ownerMale;
   public ownerFemale;
 
@@ -54,25 +50,23 @@ export class AppComponent {
   getPeople() {
     this._requestService.getPeople().subscribe(
       data => {
-      this.people = data
-       const catsFound = this.getCats(data);
-       this.ownerMale = this.getMaleOwners(catsFound);
-       this.ownerFemale = this.getFemaleOwners(catsFound);
-        // return this.pets;
+        const catsFound = this.getCats(data);
+        this.ownerMale = this.getMaleOwners(catsFound);
+        this.ownerFemale = this.getFemaleOwners(catsFound);
       },
       err => console.error(err),
-      () => console.log('done loading pets')
+      () => console.log("done loading...")
     );
   }
-  getCats(person) {
-    return this._requestService.getCats(person)
+  getCats(data) {
+    return this._requestService.getCats(data);
   }
   getMaleOwners(catsArray) {
-    return this._requestService.getMaleOwners(catsArray)
+    return this._requestService.getMaleOwners(catsArray);
   }
   getFemaleOwners(catsArray) {
-    return this._requestService.getFemaleOwners(catsArray)
+    return this._requestService.getFemaleOwners(catsArray);
   }
 
-  constructor(private _requestService: RequestService) { }
+  constructor(private _requestService: RequestService) {}
 }
--- src/app/request-service.service.ts
+++ src/app/request-service.service.ts
@@ -1,49 +1,46 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-
-
-const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-};
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable()
 export class RequestService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  // Uses http.get() to load data from a single API endpoint
   getPeople() {
-    let request = this.http.get('http://agl-developer-test.azurewebsites.net/people.json');
+    let request = this.http.get(
+      "http://agl-developer-test.azurewebsites.net/people.json"
+    );
     return request;
   }
   getCats(people) {
-    const petsFiltered = people.filter(items => items && items.pets !== null && items.pets.length > 0)
-let arrayOfPets = [];
-   (petsFiltered).map((i) => {
+    const petsFiltered = people.filter(
+      items => items && items.pets !== null && items.pets.length > 0
+    );
+    let arrayOfPets = [];
+    petsFiltered.map(i => {
       for (let petObj of i.pets) {
-        if (petObj.type === 'Cat') {
-           arrayOfPets.push({[i.gender] :petObj.name})
+        if (petObj.type === "Cat") {
+          arrayOfPets.push({ [i.gender]: petObj.name });
         }
       }
-  })
-  return arrayOfPets;
+    });
+    return arrayOfPets;
   }
   getMaleOwners(allArray) {
     let maleArray = [];
-    (allArray).map((i) => {
-        if (i.Male) {
-          maleArray.push(i.Male);
-        }
-  })
-  return maleArray.sort()
+    allArray.map(i => {
+      if (i.Male) {
+        maleArray.push(i.Male);
+      }
+    });
+    return maleArray.sort();
   }
   getFemaleOwners(allArray) {
     let femaleArray = [];
-    (allArray).map((i) => {
-        if (i.Female) {
-          femaleArray.push(i.Female);
-        }
-  })
-  return femaleArray.sort()
+    allArray.map(i => {
+      if (i.Female) {
+        femaleArray.push(i.Female);
+      }
+    });
+    return femaleArray.sort();
   }
 }
